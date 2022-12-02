f1e0c4aee68f8fa77e85b259be593ba9638b3906
sadhiin <sanjidulislamsadhin1994@gmail.com>
2022-12-03 00:27:01 +0600

    Solved the day-02

---
+++ Day-2/solution.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx,avx2,fma")
+#pragma GCC optimization("unroll-loops")
+using namespace std;
+#define ll long long
+#define ull unsigned long long
+#define vi vector<int>
+#define vll vector<ll>
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define rep(i,a,b) for(int i=a;i<b;++i)
+#define rrep(i,a,b) for(int i=a;i>=b;i--)
+#define fore(i,a) for(auto &i:a)
+#define all(x) (x).begin(),(x).end()
+#define min3(a,b,c) min(a,min(b,c))
+#define max3(a,b,c) max(a,max(b,c))
+#define dbg(x)      cout << #x <<" "<< x << endl;
+#define re return
+#define en cout<<"\n";
+const int MOD=1e9+7;
+const int MAXN=1e5+105;
+
+
+void solve(){
+
+
+
+}
+int32_t main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);cout.tie(0);
+
+
+#ifndef ONLINE_JUDGE
+freopen("input.txt", "r", stdin);
+freopen("output.txt", "w", stdout);
+#endif
+    solve();
+    return 0;
+}
